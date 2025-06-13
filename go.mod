126d7420bafc262bc97b914c94760a414b2b55a1
KasumiMercury <naoki_shira@outlook.jp>
2025-06-13 22:01:42 +0900

    [WIP] request handler initialize

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module passkey-learn
 
 go 1.24
+
+require github.com/go-webauthn/webauthn v0.13.0 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"github.com/go-webauthn/webauthn/webauthn"
+	"net/http"
+)
+
+var (
+	WebAuthn *webauthn.WebAuthn
+	err      error
+)
 
 func main() {
+	waConfig := &webauthn.Config{
+		RPDisplayName: "WebAuthn Go Learning",
+		RPID:          "localhost",
+		RPOrigins:     []string{"http://localhost:8080"},
+	}
+
+	if WebAuthn, err = webauthn.New(waConfig); err != nil {
+		panic(err)
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("./front/")))
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
